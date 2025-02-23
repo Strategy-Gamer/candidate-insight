ab41d560c68befcb14199af090130b77d70c1fcd
bbentley4 <brennafierro@gmail.com>
2025-02-23 15:14:50 -0500

    provided real image to mock data for testing.

--- database/mockdata.sql
+++ database/mockdata.sql
@@ -1,7 +1,7 @@
 -- This file contains mock data for the database tables to be used for testing and development purposes.
 INSERT INTO Candidate (first_name, last_name, ethnicity, gender, party_affiliation, state, profile_image_url, congressional_district, website_url, dob)
 VALUES
-('John', 'Doe', 'Caucasian', 'Male', 'Independent', 'NY', 'https://example.com/john_doe.jpg', 'NY-10', '1975-06-15'),
+('John', 'Doe', 'Caucasian', 'Male', 'Independent', 'NY', 'https://drive.google.com/file/d/1-njNjTEFghN_VpVldqHwOcEaBMQXOFeE/view?usp=share_link', 'NY-10', '1975-06-15'),
 ('Jane', 'Smith', 'Hispanic', 'Female', 'Democrat', 'CA', 'https://example.com/jane_smith.jpg', 'CA-12', '1985-04-22'),
 ('Robert', 'Johnson', 'African American', 'Male', 'Republican', 'TX', 'https://example.com/robert_johnson.jpg', 'TX-07', '1970-09-12');
 
