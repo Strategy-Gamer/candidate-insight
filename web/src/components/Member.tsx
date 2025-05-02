bbde7c97a18c3cdf6a9e5567953bed199efef7f0
10-Squares <tensquares@hotmail.com>
2025-05-02 11:42:52 -0400

    Add two row about us page :(

--- web/src/components/Member.tsx
+++ web/src/components/Member.tsx
@@ -21,10 +21,10 @@ const Member: React.FC<MemberProps> = ({firstName, lastName, role, githubUrl, li
 
                <div className="social-links">
                     <Link href={githubUrl}>
-                         <Image src="images/github.svg" alt="Github Icon" height="100" width="100" className="media-icon"></Image>
+                         <Image src="images/github.svg" alt="Github Icon" height="50" width="50" className="media-icon"></Image>
                     </Link>
                     <Link href={linkedinUrl}>
-                         <Image src="images/linkedin.svg" alt="Linkedin Icon" height="100" width="100" className="media-icon"></Image>
+                         <Image src="images/linkedin.svg" alt="Linkedin Icon" height="50" width="50" className="media-icon"></Image>
                     </Link>
                </div>
           </div>
