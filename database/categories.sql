155c78a474ae69ed516b4b71bd184473b4c0433b
Logan Caraway <71653916+logannick02@users.noreply.github.com>
2025-05-02 22:39:18 -0400

    Update categories.sql

--- database/categories.sql
+++ database/categories.sql
@@ -4,7 +4,7 @@ VALUES
   ('Economy', 'Issues regarding taxation, employment, and business.', 'economy_icon.png'),
   ('Environment', 'Policies addressing climate change and sustainability.', 'environment_icon.png'),
   ('Education', 'Funding and reforms for public and private education.', 'education_icon.png'),
-  ('Gun Control', 'Regulations regarding firearm ownership and use.', 'gun_control_icon.png'),
+  ('Civil Liberties', 'Issues related to civil liberties like gun ownership', 'gun_control_icon.png'),
   ('Foreign Policy', 'Diplomatic and military strategies.', 'foreign_policy_icon.png'),
   ('Immigration', 'Policies on border security and citizenship.', 'immigration_icon.png'),
   ('Social Issues', 'Civil rights, LGBTQ+ rights, and religious freedoms.', 'social_issues_icon.png');
